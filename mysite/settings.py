aec464a911cca0ae68fb9275724b295a899e6d1b
Moritz Frisch <mo_68@gmx.de>
2024-01-24 17:39:46 +0100

    config changed to allow hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = "django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = [
-    "*.railway.app",
-    "*.ruehl-photo.de",
+    ".railway.app",
+    ".ruehl-photo.de",
     "localhost",
 ]
 
