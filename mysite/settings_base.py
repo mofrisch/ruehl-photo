9e7b67476aee8557a4028b19a9e350aa4ff300c5
Moritz Frisch <mo_68@gmx.de>
2024-02-01 10:14:38 +0100

    change

--- mysite/settings_base.py
+++ mysite/settings_base.py
@@ -82,6 +82,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
