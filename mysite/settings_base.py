de6a25efc29d7fc29614a41823ee0bc860a7f428
Moritz Frisch <mo_68@gmx.de>
2024-02-01 08:50:44 +0100

    change

--- mysite/settings_base.py
+++ mysite/settings_base.py
@@ -79,10 +79,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/app/statifiles/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
