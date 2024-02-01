a7b36819f77bc35229d94e82b8c850bd4880a3b6
Moritz Frisch <mo_68@gmx.de>
2024-02-01 09:37:31 +0100

    change 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,8 @@
 from .settings_base import *
+from pathlib import Path
 
-import os
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -10,34 +12,17 @@ import os
 SECRET_KEY = "django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", False)
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".railway.app",
-    ".ruehl-photo.de",
-    "localhost",
-]
-
-# FORM SUBMISSION
-# Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = [
-    "https://test.ruehl-photo.de",
-    "https://ruehl-photo.de",
-    "http://localhost:8000",
-    "https://www.ruehl-photo.de",
-]
+ALLOWED_HOSTS = []
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+#
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ["PGDATABASE"],
-        "USER": os.environ["PGUSER"],
-        "PASSWORD": os.environ["PGPASSWORD"],
-        "HOST": os.environ["PGHOST"],
-        "PORT": os.environ["PGPORT"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
