d048365b5b57130ffd4b245d64e90b48d63ee9a0
Moritz Frisch <mo_68@gmx.de>
2024-02-09 18:17:28 +0100

    image cdn cloudflare

--- pages/templatetags/picture.py
+++ pages/templatetags/picture.py
@@ -4,51 +4,30 @@ register = template.Library()
 
 
 @register.inclusion_tag("picture.html")
-def picture(name, design_width, design_height, large=False, **sizes_input):
-    widths = range(100, 2100, 100)
-    extra_widths = range(2200, 4001, 200)
-    if large:
-        widths = [*widths, *extra_widths]
+def picture(name, id, design_width, design_height, widths="", sizes=""):
+    img_path = "https://imagedelivery.net/JfsbyIUZYacdu7Ymh8jh3A/"
+    pic_path = img_path + id
+    path = pic_path + "/w=" + str(design_width)
+    if widths:
+        if not sizes:
+            sizes = "(max-width: 600px) 100vw, " + str(design_width) + "px"
 
-    avif_urls = [
-        ["img/gen/" + name + "-" + str(width) + ".avif", width] for width in widths
-    ]
-    webp_urls = [
-        ["img/gen/" + name + "-" + str(width) + ".webp", width] for width in widths
-    ]
-    jpeg_urls = [
-        ["img/gen/" + name + "-" + str(width) + ".jpg", width] for width in widths
-    ]
-    img_url = "img/gen/" + name + "-" + str(design_width) + ".jpg"
+    gen_widths = []
+    if widths:
+        gen_widths = widths.split(",")
 
-    BREAKPOINTS = {
-        "xs": 0,
-        "sm": 576,
-        "md": 768,
-        "lg": 992,
-        "xl": 1200,
-        "xxl": 1400,
-    }
-
-    breakpoints = {}
-
-    for key, value in sizes_input.items():
-        if key in BREAKPOINTS:
-            breakpoints[key] = value
-
-    sizes = ""
-
-    for key, value in breakpoints.items():
-        sizes += "(min-width: " + str(BREAKPOINTS[key]) + "px) " + str(value) + "px, "
-    sizes += "calc(100vw - 24px)"
+    print(name + ": " + path)
+    print(gen_widths)
+    print("\n")
+    srcset = ""
+    for width in gen_widths:
+        srcset += pic_path + "/w=" + width + " " + width + "w, "
 
     return {
         "name": name,
+        "path": path,
         "design_width": design_width,
         "design_height": design_height,
-        "avif_urls": avif_urls,
-        "webp_urls": webp_urls,
-        "jpeg_urls": jpeg_urls,
-        "img_url": img_url,
+        "srcset": srcset,
         "sizes": sizes,
     }
